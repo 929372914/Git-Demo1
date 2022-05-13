46fee29fc8e9ccb4839a558c24483e701723be2a
lenovo <929372914@qq.com>
2022-05-13 11:48:42 +0800

    hello3

--- src/main/java/com/duing/controller/TestController.java
+++ src/main/java/com/duing/controller/TestController.java
@@ -11,6 +11,6 @@ public class TestController {
     @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "hello word3";
+        return "hello word 3";
     }
 }
